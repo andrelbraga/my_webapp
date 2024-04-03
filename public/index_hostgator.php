3f4ea6eb522fae535e0105fac0803571a6f57648
André Braga <andre.braga@olxbr.com>
2024-04-03 01:31:02 -0300

    Alteracao deploy task

--- public/index_hostgator.php
+++ public/index_hostgator.php
@@ -2,8 +2,7 @@
 
 use App\Kernel;
 
-require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
-#require_once 'vendor/autoload_runtime.php'; ##deploy hostgator
+require_once 'vendor/autoload_runtime.php'; ##deploy hostgator
 
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
